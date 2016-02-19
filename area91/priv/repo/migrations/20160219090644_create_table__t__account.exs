9c04b28bf926d1e5c2057a0cda16de8b9dc30efe
Andy Nagels <thereisanewway@gmail.com>
2016-02-19 12:33:44 +0100

    Added a migration for creating the T_ACCOUNT table.

---
+++ area91/priv/repo/migrations/20160219090644_create_table__t__account.exs
@@ -0,0 +1,24 @@
+defmodule Area91.Repo.Migrations.CreateTable_T_ACCOUNT do
+  use Ecto.Migration
+
+  def up do
+    "CREATE TABLE T_ACCOUNT
+    (
+        account_id serial not null,
+        name varchar(4000) not null,
+        description varchar(4000) not null,
+        is_active int not null default 1,
+        date_created timestamp not null default '1900-01-01',
+        date_modified timestamp not null default '1900-01-01',
+        constraint pk_account_id primary key(account_id)
+    );" 
+  end
+
+  def down do
+   "drop table T_ACCOUNTS;" 
+  end
+
+
+  def change do
+  end
+end
