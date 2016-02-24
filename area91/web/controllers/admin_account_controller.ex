2089639715eaea9bb7430182249746532d118120
Andy Nagels <thereisanewway@gmail.com>
2016-02-24 14:07:04 +0100

    New account creationg code added.

--- area91/web/controllers/admin_account_controller.ex
+++ area91/web/controllers/admin_account_controller.ex
@@ -1,9 +1,25 @@
 defmodule Area91.AdminAccountController do
   use Phoenix.Controller
 
+  alias Area91.Router
+  import Area91.Router.Helpers
+
   plug :action
 
   def account(conn, _params) do
-    render conn, "account.html"
+    conn
+    |> assign(:accounts, Area91.Repo.all(Area91.Account))
+    |> render(conn, "account.html")
   end
+
+  def new(conn, _params) do
+    render(conn, "new.html")
+  end
+
+  def create(conn, %{"account" => %{"name" => name, "description" => description}}) do
+    l_account = %Area91.Account{name: name, description: description}
+    Area91.Repo.insert(l_account)
+    redirect(conn, to: admin_account_path(conn, :overview))
+  end
+
 end
